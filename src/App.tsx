987e76a969a985af9f25f7252592d357143d06c9
SEONGMINY <leesm2075@naver.com>
2024-11-25 00:36:17 +0900

    feat: App Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import React, { lazy, Suspense } from 'react';
 import { Html } from './Html';
+
+const Main = lazy(() => import('./components/Main.jsx'));
 
 const App = () => {
   return (
@@ -6,6 +9,9 @@ const App = () => {
       <div>
         <h1>서버 사이드 렌더링 예제</h1>
         <p>이 페이지는 서버에서 렌더링되었습니다.</p>
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <Main />
+        </Suspense>
       </div>
     </Html>
   );
