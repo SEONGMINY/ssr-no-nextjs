c9e259fdeb75daf67167d40778f7a6bfec962685
SEONGMINY <leesm2075@naver.com>
2024-11-25 00:35:12 +0900

    feat: App Html 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { Html } from './Html';
 
 const App = () => {
   return (
-    <div>
-      <h1>서버 사이드 렌더링 예제</h1>
-      <p>이 페이지는 서버에서 렌더링되었습니다.</p>
-    </div>
+    <Html>
+      <div>
+        <h1>서버 사이드 렌더링 예제</h1>
+        <p>이 페이지는 서버에서 렌더링되었습니다.</p>
+      </div>
+    </Html>
   );
 };
 
